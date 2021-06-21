de05baf71c1da23eb71a115cc515862dcadbef51
urielkodia <85808875+urielkodia@users.noreply.github.com>
2021-06-21 15:00:53 -0500

    Add files via upload

---
+++ hello-local/scripts/script.js
@@ -0,0 +1,12 @@
+//jQuery
+$(document).ready(function () {
+    console.log("Hello, CodePen!");
+    //JavaScript
+    // Select the element and store it in a variable.
+    var el = document.getElementById("my-greeting");
+    // Change the value of the class attribute.
+    el.innerText = "hello, github! (programtically.)";
+
+    //let's do it again with jQ
+    //$("#my-greeting").text("hello again");
+});
